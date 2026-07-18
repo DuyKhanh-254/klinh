677ba38bee539b80aff59dfab4aeaf1b6df0f9fb
Nguyễn Duy Khánh <ndk254007@gmail.com>
2026-07-18 22:47:33 +0700

    Use custom History Nineties font

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "@fontsource-variable/baloo-2/index.css";
 import "@fontsource-variable/nunito/index.css";
-import "@fontsource-variable/playfair-display/index.css";
 import App from "./App";
 import "./styles.css";
 
